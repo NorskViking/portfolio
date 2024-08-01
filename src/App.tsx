b51b70804d0ad62586772f801a3f79e5e2c6689d
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-08-01 15:23:36 +0200

    Testing dark|light mode
    Testing implementing Bootstrap dark|light mode toggle.

--- src/App.tsx
+++ src/App.tsx
@@ -15,24 +15,10 @@ function App() {
 
   return (
     <div className={`d-flex flex-column justify-content-center align-items-center ${isDarkMode ? 'bg-dark text-light' : 'bg-light'}`} >
-      <header className="App-header">
-        <Navbar />
-        <Button variant="primary" onClick={toggleDarkMode}>
+      <Navbar />
+      <Button variant="primary" onClick={toggleDarkMode}>
           Toggle theme
-        </Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Button>
     </div>
   );
 }
