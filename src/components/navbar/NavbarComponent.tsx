41b89cf37b315110139507f55fa962599eecea8a
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-08-21 14:22:28 +0200

    Update NavbarComponent.tsx
    Added icons to nav.links

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -8,6 +8,7 @@ import SettingsButton from "../buttons/SettingsButton";
 // Import of assets
 import logoLight from "../../assets/png/Logo - LightBlue.png"
 import logoDark from "../../assets/png/Logo - DarkBlue.png"
+import "bootstrap-icons/font/bootstrap-icons.css";
 // Import of Redux logic
 import { useSelector } from "react-redux";
 import { Rootstate } from "../../store/rootReducer";
@@ -41,9 +42,18 @@ function NavbarComponent() {
                 </Navbar.Brand>
                 <Nav className="d-flex align-items-center justify-content-end">
                     <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto"/>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#landingPage">
+                        <i className="bi bi-house-fill"> </i>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link href="#about">
+                        <i className="bi bi-person-circle"> </i>
+                        About
+                    </Nav.Link>
+                    <Nav.Link href="#projects">
+                        <i className="bi bi-journal-code"> </i>
+                        Projects
+                    </Nav.Link>
                     <Nav.Item>
                         <SettingsButton />
                     </Nav.Item>
