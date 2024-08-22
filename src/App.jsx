f6723c0e6c4d3993e02120043a51ce43de34e5a4
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-08-22 15:42:22 +0200

    compile scss to css
    Trying to fix issue with custom.scss not beeing loaded due to MIME type.
    Trying to fix issue with color of modal and card is transparent.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { Rootstate } from "./store/rootReducer"
 import Container from "react-bootstrap/Container";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/custom.scss';
+import './styles/_custom.scss';
 
 import NavbarComponent from './components/navbar/NavbarComponent';
 import LandingPage from './pages/LandingPage';
